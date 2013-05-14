15beb2cc1a5251a479c6a31ab20b4418d9026b3e
Luca Orlandi <luca.orlandi@gmail.com>
2013-05-14 15:31:44 +0200

    Regenerated unit-tests

--- test/unit/it/lrkwz/school/library/SchoolTests.groovy
+++ test/unit/it/lrkwz/school/library/SchoolTests.groovy
@@ -1,17 +1,26 @@
 package it.lrkwz.school.library
 
-
+import static org.junit.Assert.*
 
 import grails.test.mixin.*
+import grails.test.mixin.support.*
 import org.junit.*
 
 /**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
-@TestFor(School)
+@TestMixin(GrailsUnitTestMixin)
 class SchoolTests {
 
+    void setUp() {
+        // Setup logic here
+    }
+
+    void tearDown() {
+        // Tear down logic here
+    }
+
     void testSomething() {
-       fail "Implement me"
+        fail "Implement me"
     }
 }
