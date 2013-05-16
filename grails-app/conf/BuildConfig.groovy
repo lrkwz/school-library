e86f5025d1257d14240ba58613984910cba0bc89
Luca Orlandi <luca.orlandi@gmail.com>
2013-05-16 15:22:53 +0200

    Fixing 'No thread-bound request found' as for http://grails.1312388.n4.nabble.com/No-thread-bound-request-found-error-td4631072.html

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,62 +7,63 @@ grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
+	// inherit Grails' default dependencies
+	inherits("global") {
+		// uncomment to disable ehcache
+		// excludes 'ehcache'
+	}
+	log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	checksums true // Whether to verify checksums on resolve
 
-    repositories {
-        inherits true // Whether to inherit repository definitions from plugins
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
-        mavenCentral()
+	repositories {
+		inherits true // Whether to inherit repository definitions from plugins
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+		mavenCentral()
 
-        // uncomment these to enable remote dependency resolution from public Maven repositories
-        //mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+		// uncomment these to enable remote dependency resolution from public Maven repositories
+		//mavenCentral()
+		//mavenLocal()
+		//mavenRepo "http://snapshots.repository.codehaus.org"
+		//mavenRepo "http://repository.codehaus.org"
+		//mavenRepo "http://download.java.net/maven/2/"
+		//mavenRepo "http://repository.jboss.com/maven2/"
+	}
+	dependencies {
+		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.16'
-    }
+		// runtime 'mysql:mysql-connector-java:5.1.16'
+	}
 
-    plugins {
+	plugins {
 		compile ":fields:1.3"
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.7.2"
-        runtime ":resources:1.1.6"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
+		runtime ":hibernate:$grailsVersion"
+		runtime ":jquery:1.7.2"
+		runtime ":resources:1.1.6"
+		// Uncomment these (or add new ones) to enable additional resources capabilities
+		//runtime ":zipped-resources:1.0"
+		//runtime ":cached-resources:1.0"
+		//runtime ":yui-minify-resources:0.1.4"
 
-        build ":tomcat:$grailsVersion"
-		
+		build ":tomcat:$grailsVersion"
+
 		// Responsive
 		runtime ":twitter-bootstrap:2.3.0"
-		
+
 		// Tagging
 		compile ":taggable:1.0.1"
 		compile ":tagcloud:0.3"
-		
+
 		// Security
 		compile ':spring-security-core:1.2.7.3'
 		compile ":spring-security-ui:0.2"
 		compile ":jquery-ui:1.8.24"
 		compile ":famfamfam:1.0.1"
 		compile ":mail:1.0.1"
-		
+		compile ":webxml:1.4.1"
+
 		//compile ":breadcrumbs:0.5.2"
 		compile ":bootstrap-crumbs:1.0.2"
-    }
+	}
 }
